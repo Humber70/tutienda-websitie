965f9e48608d8c06f075041977f1b3b549bb33fd
Humberto <humber.arraya@gmail.com>
2023-03-14 02:07:46 -0300

    refact

--- src/js/header.js
+++ src/js/header.js
@@ -1,41 +1,33 @@
 
  const $header = document.getElementById('data-header')
  const $main = document.getElementById("data-main")
-
-
 //Scroll
-
-
  const $bars = document.querySelector(".bar");
  const $show_active = document.querySelector(".list");
- 
 
+document.addEventListener("click", (e)=>  {
+    if(!e.target.matches(".fa-bars")){
+        $bars.classList.remove("show-list")
+    }
+    $show_active.classList.toggle("show-list")
+    $bars.classList.toggle("bar-animation")
 
-
- 
- $bars.addEventListener("click", (e)=>  {
-     
-     $show_active.classList.toggle("show-list")
-        $bars.classList.toggle("bar-animation")
-
-
-    })
-    
+})
   
-    window.addEventListener('scroll', (e) => {
-    
-       const data = $main.getBoundingClientRect()
-       if(data.top < 115) {
-           $header.style.boxShadow = "0px 0px 1px 0px #140f1b";
-           $header.style.height = "70px";
-           $show_active.classList.remove("show-list")
-            $bars.classList.remove("bar-animation")
-           
-       }else {
-           $header.style.boxShadow = "none";
-           $header.style.height = "auto";
-       } 
-    
-    
-    })
-console.log($bars.matches(".bar"))
+window.addEventListener('scroll', (e) => {
+
+    const data = $main.getBoundingClientRect()
+    if(data.top < 115) {
+        $header.style.boxShadow = "0px 0px 1px 0px #140f1b";
+        $header.style.height = "70px";
+        $show_active.classList.remove("show-list")
+        $bars.classList.remove("bar-animation")
+        
+    }else {
+        $header.style.boxShadow = "none";
+        $header.style.height = "auto";
+    } 
+
+
+})
+

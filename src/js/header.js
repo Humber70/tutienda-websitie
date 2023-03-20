9eea4ce92a0d7bc9069666ce196c88e8af368d32
Humberto <humber.arraya@gmail.com>
2023-03-20 19:48:53 -0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,33 +1,35 @@
-
- const $header = document.getElementById('data-header')
- const $main = document.getElementById("data-main")
+const $header = document.getElementById("data-header");
+const $main = document.getElementById("data-main");
 //Scroll
- const $bars = document.querySelector(".bar");
- const $show_active = document.querySelector(".list");
+const $bars = document.querySelector(".bar");
+const $show_active = document.querySelector(".list");
+const $paren_list = $show_active.parentElement;
 
-document.addEventListener("click", (e)=>  {
-    if(!e.target.matches(".fa-bars")){
-        $bars.classList.remove("show-list")
-    }
-    $show_active.classList.toggle("show-list")
-    $bars.classList.toggle("bar-animation")
+//funciones
+const navigator = (e) => {
+  if (e.target.classList.contains("fa-bars")) {
+    $show_active.classList.toggle("show-list");
+    e.target.classList.toggle("bar-animation");
+  }
+};
 
-})
-  
-window.addEventListener('scroll', (e) => {
-
-    const data = $main.getBoundingClientRect()
-    if(data.top < 115) {
-        $header.style.boxShadow = "0px 0px 1px 0px #140f1b";
-        $header.style.height = "70px";
-        $show_active.classList.remove("show-list")
-        $bars.classList.remove("bar-animation")
-        
-    }else {
-        $header.style.boxShadow = "none";
-        $header.style.height = "auto";
-    } 
+const scrollHeader = (e) => {
+  const data = $main.getBoundingClientRect();
+  if (data.top < 115) {
+    $header.style.boxShadow = "0px 0px 1px 0px #140f1b";
+    $header.style.height = "70px";
+    $show_active.classList.remove("show-list");
+    $bars.classList.remove("bar-animation");
+  } else {
+    $header.style.boxShadow = "none";
+    $header.style.height = "auto";
+  }
+};
 
 
-})
+
+//Events
+$bars.addEventListener("click", navigator);
+window.addEventListener("scroll", scrollHeader);
+
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,86 +1,71 @@
-import productos from "./db.json" assert {type: "json"}
+import productos from "./db.json" assert { type: "json" };
 
-document.addEventListener('DOMContentLoaded', () => {
-    getProductos();
-    footerSocial();
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+  getProductos();
+  footerSocial();
+});
 const d = document;
-const $listProduct = d.getElementById('list-product');
-const $template = d.getElementById('template').content;
+const $listProduct = d.getElementById("list-product");
+const $template = d.getElementById("template").content;
 const $fragment = d.createDocumentFragment();
-const $list = d.getElementById('list-product')
+const $list = d.getElementById("list-product");
 
 //elementos template
 const image = $template.querySelector("img");
-const h3 = $template.querySelector('h3');
-const h2 = $template.querySelector('h2');
-const button =  $template.querySelector("button");
-const parrafo = $template.querySelector('p');
+const h3 = $template.querySelector("h3");
+const h2 = $template.querySelector("h2");
+const button = $template.querySelector("button");
+const parrafo = $template.querySelector("p");
 
-const $redes = d.querySelector(".redes")
-const $icon_redes = $redes.querySelectorAll("i")
-const $icon_red = [...$icon_redes]
+const $redes = d.querySelector(".redes");
+const $icon_redes = $redes.querySelectorAll("i");
+const $icon_red = [...$icon_redes];
 //boton
-const $btnIfo = d.getElementById("btn-info")
+const $btnIfo = d.getElementById("btn-info");
 //parent
 const $footer = d.querySelector(".footer");
 //brother
 const $btnIfo_icon = $btnIfo.firstElementChild.nextElementSibling;
 const $footer_info = $footer.firstElementChild;
 
-$list.addEventListener('click', mouseIn)
-$btnIfo.addEventListener("click", moreInfo)
-window.addEventListener("resize", footerSocial)
+$list.addEventListener("click", mouseIn);
+$btnIfo.addEventListener("click", moreInfo);
+window.addEventListener("resize", footerSocial);
 
-function mouseIn (e) {
-    e.preventDefault();
-    
-    if(e.target.classList.contains("pa")) {
-        e.target.classList.toggle("show")
+//functions
+function mouseIn(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("pa")) {
+    e.target.classList.toggle("show");
+  }
+}
+function moreInfo() {
+  $footer_info.classList.toggle("active-btn");
+  $btnIfo_icon.classList.toggle("icon-reverse");
+}
+function footerSocial() {
+  for (const i of $icon_red) {
+    if (window.screen.width < 420) {
+      i.textContent = "";
     }
-
-}
-function moreInfo () {
-    
-    $footer_info.classList.toggle("active-btn")
-    $btnIfo_icon.classList.toggle("icon-reverse")
-}
-function footerSocial () {
-    for( const i of $icon_red) {
-        if(window.screen.width < 420) {
-            i.textContent = "";
-        }
-    }
+  }
 }
 
-function getProductos (name,url,description,price) {
+function getProductos(name, url, description, price) {
+  image.setAttribute("src", url);
+  image.setAttribute("alt", name);
+  h2.textContent = price;
+  h3.textContent = name;
+  parrafo.textContent = description;
+  button.textContent = "agregar al carro";
 
-    image.setAttribute("src", url);
-    image.setAttribute("alt", name);
-    h2.textContent = price;
-    h3.textContent = name;
-    parrafo.textContent = description;
-    button.textContent = "agregar al carro";
-    
-    const $clone = d.importNode($template, true);
-    $fragment.appendChild($clone);
+  const $clone = d.importNode($template, true);
+  $fragment.appendChild($clone);
 }
 
-productos.forEach( element => {
-    let {name,url,description,price} = element;
-    getProductos(name,url,description,price)
-    $listProduct.appendChild($fragment);   
-})
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+productos.forEach((element) => {
+  let { name, url, description, price } = element;
+  getProductos(name, url, description, price);
+  $listProduct.appendChild($fragment);
+});
